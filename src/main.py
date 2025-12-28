3a4330b80b2d7394d81d42ea9fbdc6ef8ea5bf77
spline2hg <181270613+spline2hg@users.noreply.github.com>
2025-12-28 15:35:22 +0000

    feat: allow all HTTP methods for CORS

--- src/main.py
+++ src/main.py
@@ -31,7 +31,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=cors_origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
